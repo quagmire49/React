7942783457fb4f040cad3e1fd8559352ac5612c0
quagmire49 <geeteshkovel@gmail.com>
2026-07-04 11:38:06 +0530

    learning props basics

---
+++ props/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Box from './Box';
+
+function App() {
+  return (
+    <>
+    <Box/>
+    <Box name="Gennady" profession="Cp coder"/>
+    </>
+  )
+}
+
+export default App
